b5d46bc20bfcd66eb7faa035a66dcb612f4a0ffd
Triston117 <roviiknaris@yahoo.com>
2023-06-12 15:36:49 -0700

    Stable react app

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from "react";
 
 const App = () => {
   const [tasks, setTasks] = useState([]);
+  const [newTask, setNewTask] = useState("");
 
   useEffect(() => {
     fetch("/api/tasks")
@@ -14,12 +15,57 @@ const App = () => {
       });
   }, []);
 
+  const handleTaskCreate = () => {
+    fetch("/api/tasks", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        description: newTask,
+      }),
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        setTasks([...tasks, data]);
+        setNewTask("");
+      })
+      .catch((error) => {
+        console.error("ERROR", error);
+      });
+  };
+
+  const handleTaskDelete = (taskId) => {
+    fetch(`/api/tasks/${taskId}`, {
+      method: "DELETE",
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        const updatedTasks = tasks.filter((task) => task.id !== data.id);
+        setTasks(updatedTasks);
+      })
+      .catch((error) => {
+        console.error("ERROR", error);
+      });
+  };
+
   return (
     <main>
+      <div className="task-form">
+        <input
+          type="text"
+          placeholder="New Task"
+          value={newTask}
+          onChange={(e) => setNewTask(e.target.value)}
+        />
+        <button onClick={handleTaskCreate}>Create Task</button>
+      </div>
+
       {tasks.map((task) => (
-        <span className="task" key={task.id}>
-          {task.description}
-        </span>
+        <div className="task" key={task.id}>
+          <span>{task.description}</span>
+          <button onClick={() => handleTaskDelete(task.id)}>Delete</button>
+        </div>
       ))}
     </main>
   );
--- server/index.js
+++ server/index.js
@@ -26,6 +26,73 @@ app.get("/api/tasks", (req, res) => {
   });
 });
 
+app.post("/api/tasks", (req, res) => {
+  const { title, description } = req.body;
+  pool.query(
+    "INSERT INTO tasks (title, description) VALUES ($1, $2) RETURNING *",
+    [title, description],
+    (error, result) => {
+      if (error) {
+        console.error("Error executing query:", error);
+        res.status(500).send("Internal Server Error");
+      } else {
+        res.send(result.rows[0]);
+      }
+    }
+  );
+});
+
+app.get("/api/tasks/:id", (req, res) => {
+  const taskId = req.params.id;
+  pool.query("SELECT * FROM tasks WHERE id = $1", [taskId], (error, result) => {
+    if (error) {
+      console.error("Error executing query:", error);
+      res.status(500).send("Internal Server Error");
+    } else if (result.rows.length === 0) {
+      res.status(404).send("Task not found");
+    } else {
+      res.send(result.rows[0]);
+    }
+  });
+});
+
+app.put("/api/tasks/:id", (req, res) => {
+  const taskId = req.params.id;
+  const { title, description } = req.body;
+  pool.query(
+    "UPDATE tasks SET title = $1, description = $2 WHERE id = $3 RETURNING *",
+    [title, description, taskId],
+    (error, result) => {
+      if (error) {
+        console.error("Error executing query:", error);
+        res.status(500).send("Internal Server Error");
+      } else if (result.rows.length === 0) {
+        res.status(404).send("Task not found");
+      } else {
+        res.send(result.rows[0]);
+      }
+    }
+  );
+});
+
+app.delete("/api/tasks/:id", (req, res) => {
+  const taskId = req.params.id;
+  pool.query(
+    "DELETE FROM tasks WHERE id = $1 RETURNING *",
+    [taskId],
+    (error, result) => {
+      if (error) {
+        console.error("Error executing query:", error);
+        res.status(500).send("Internal Server Error");
+      } else if (result.rows.length === 0) {
+        res.status(404).send("Task not found");
+      } else {
+        res.send(result.rows[0]);
+      }
+    }
+  );
+});
+
 app.listen(PORT, () => {
   console.log(`Listening on port ${PORT}`);
 });
